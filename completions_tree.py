1c3bcb4c3fd057ae333b385a923465489be57c7a
summerstay <jsummerstay@gmail.com>
2020-03-15 10:43:06 -0400

    Add files via upload

---
+++ completions_tree.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 14 16:16:13 2020
+@author: Doug Summers Stay
+Find the most probable completions of a sentence using gpt-2.
+"""
+
+import torch
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
+import torch.nn.functional as F
+
+def grow_branches(sentence_so_far, probs, input_probability,past, h):
+    #recursive function to find all sentence completions
+    global branch_list
+    global leaf_list
+    global complete_list
+    global model
+    sorted_probability_list = sorted(enumerate(probs), key=lambda x: x[1], reverse=True)
+    has_children = False
+    for (this_token,this_probability) in sorted_probability_list:
+        next_probability = this_probability * input_probability
+        out_sentence = sentence_so_far.copy()
+        sentence_and_probability = (out_sentence, input_probability)
+        if this_token == 13:
+            complete_list.append(sentence_and_probability)
+            return
+        if next_probability < h:            
+            if has_children == True:
+                branch_list.append(sentence_and_probability)
+            else:
+                leaf_list.append(sentence_and_probability)
+            return
+        else:
+            has_children = True
+            next_sentence = sentence_so_far.copy()
+            next_sentence.append(this_token)
+            (next_probability_list,next_past) = expand_node(next_sentence,past)
+            grow_branches(next_sentence,next_probability_list, next_probability, next_past, h)
+
+def expand_node(sentence, past):
+    #finds probabilities for the next token using gpt-2
+    global model
+    if past == None:
+        input_ids = torch.tensor(sentence).unsqueeze(0)
+    else:
+        input_ids = torch.tensor([sentence[-1]]).unsqueeze(0)
+    inputs = {'input_ids': input_ids}    
+    with torch.no_grad():
+        logits, past = model(**inputs, past=past)   
+        logits = logits[:, -1, :]  
+        probs = F.softmax(logits, dim=-1).tolist()[0]
+        return (probs, past)
+
+# globals here
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+model = GPT2LMHeadModel.from_pretrained('gpt2')
+leaf_list = []
+branch_list = []
+complete_list = []
+
+while True:
+    probability_threshhold=float(input("probability cutoff (e.g. .001 or less):"))
+    raw_prompt = input("partial sentence to complete:")
+    
+    prompt=tokenizer.encode(raw_prompt)
+    
+    (probs, past) = expand_node(prompt, None) 
+    grow_branches(prompt,probs,1,past,probability_threshhold)
+    
+    sorted_complete_list = sorted(complete_list, reverse=True,key=lambda x: x[1])
+    sorted_leaf_list = sorted(leaf_list, reverse=True,key=lambda x: x[1])
+    sorted_branch_list = sorted(branch_list, reverse=True,key=lambda x: x[1])
+    
+    # to get the most probable completed sentence:
+    # tokenizer.decode(sorted_complete_list[0])
+    
+    #print just the completions
+    for (sentence, prob) in sorted_complete_list:
+        #print(round(prob,6),end=':')
+        print(repr(tokenizer.decode(sentence[len(prompt):])).strip("'"),end='|')
+
+
